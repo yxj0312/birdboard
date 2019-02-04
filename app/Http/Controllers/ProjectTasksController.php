39852b7e470aea51ec0afa612abd219b8b8334fa
yxj0312 <yanxiajun@hotmail.com>
2019-02-04 20:16:30 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -10,7 +10,7 @@ class ProjectTasksController extends Controller
     public function store(Project $project)
     {
         request()->validate(['body' => 'required']);
-        
+
         $project->addTask(request('body'));
 
         return redirect($project->path());
