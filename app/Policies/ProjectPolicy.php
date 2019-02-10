94e4d1c0609ea875724d74643c20eed088d7acbc
yxj0312 <yanxiajun@hotmail.com>
2019-02-10 20:49:01 +0000

    Apply fixes from StyleCI

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -3,8 +3,8 @@
 namespace App\Policies;
 
 use App\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 use App\Project;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ProjectPolicy
 {
