4044ee3b9f2547b570ef98e8456b9aea44f51359
yxj0312 <yanxiajun@hotmail.com>
2019-02-16 21:39:54 +0100

    Ep 22 Project Activity Feeds: Part 3

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -75,12 +75,52 @@ class ProjectTasksTest extends TestCase
             ->actingAs($project->owner)
             ->patch($project->tasks[0]->path(), [
                 'body' => 'changed',
+            ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+        ]);
+    }
+
+    /** @test */
+    public function a_task_can_be_completed()
+    {
+        $project = ProjectFactory::withTasks(1)->create();
+
+        $this
+            ->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'changed',
                 'completed' => true,
             ]);
 
         $this->assertDatabaseHas('tasks', [
             'body' => 'changed',
             'completed' => true,
+        ]);
+    }
+
+    /** @test */
+    public function a_task_can_marked_as_incomplete()
+    {
+        $project = ProjectFactory::withTasks(1)->create();
+
+        $this
+            ->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'changed',
+                'completed' => true,
+            ]);
+
+        $this
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'changed',
+                'completed' => false,
+            ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+            'completed' => false,
         ]);
     }
 
