32de780b62d10b442ea67138ad50366f30efb23b
yxj0312 <yanxiajun@hotmail.com>
2019-02-20 21:38:13 +0100

    Ep 26 Recording Model Changes

--- app/Project.php
+++ app/Project.php
@@ -8,6 +8,8 @@ class Project extends Model
 {
     protected $guarded = [];
 
+    public $old = [];
+
     public function path()
     {
         return "/projects/{$this->id}";
@@ -35,11 +37,30 @@ class Project extends Model
      */
     public function recordActivity($description)
     {
-        $this->activity()->create(compact('description'));
+        $this->activity()->create([
+            'description' => $description,
+            'changes' => $this->activityChanges($description)
+        ]);
     }
 
     public function activity()
     {
         return $this->hasMany(Activity::class)->latest();
+    }
+
+    /**
+     * Fetch the changes to the model.
+     *
+     * @param  string $description
+     * @return array|null
+     */
+    protected function activityChanges($description)
+    {
+        if ($description == 'updated') {
+            return [
+                'before' => array_except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
+                'after' => array_except($this->getChanges(), 'updated_at')
+            ];
+        }
     }
 }
