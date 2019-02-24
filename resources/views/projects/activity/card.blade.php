b261b7e48e494797399d13e9c6fd92e64c13ba68
yxj0312 <yanxiajun@hotmail.com>
2019-02-24 22:01:39 +0100

    Ep 28 Tweaking the Activity Card 3:00

--- resources/views/projects/activity/card.blade.php
+++ resources/views/projects/activity/card.blade.php
@@ -2,7 +2,7 @@
     <ul class="text-xs list-reset">
         @foreach ($project->activity as $activity)
         <li class="{{ $loop->last ? '' : 'mb-1'}} }}">
-    @include("projects.activity.{$activity->description}")
+        @include("projects.activity.{$activity->description}")
             <span class="text-grey">{{ $activity->created_at->diffForHumans(null, true) }}</span>
         </li>
         @endforeach
