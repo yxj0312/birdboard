edbd7ad819402de9bd0b60966170d5d89dc324bc
yxj0312 <yanxiajun@hotmail.com>
2019-03-14 20:59:28 +0100

    Ep 30 Layered Tests and Invitations 2:00

---
+++ tests/Feature/InvitationsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Facades\Tests\Setup\ProjectFactory; 
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InvitationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_project_can_invite_a_user()
+    {
+        // Give I have a project
+        $project = ProjectFactory::create();
+
+        // And the owner of the project invites another user
+        $project->invite($anotherUser = factory(\App\User::class)->create());
+
+        // Then, that new user will have permission to add tasks
+        
+    }
+}
