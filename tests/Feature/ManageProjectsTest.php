f4ddad3a12d2c04ad9ef523a2c85d8d882de5f78
yxj0312 <yanxiajun@hotmail.com>
2019-03-15 22:02:48 +0100

    Ep 31 Beware False Positives 01:47

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -50,6 +50,23 @@ class ManageProjectsTest extends TestCase
     }
 
     /** @test */
+    function a_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
+    {
+        // given we're signed in
+        $user = $this->signIn();
+
+        // and we've been invited to a project that was not by created by us
+        $project = ProjectFactory::create();
+
+        $project->invite($user);
+
+        // when I visit my dashboard
+        // I should see that project
+        $this->get('/projects')
+            ->assertSee($project->title);
+    }
+
+    /** @test */
     public function unauthorized_cannot_delete_projects()
     {
         $project = ProjectFactory::create();
