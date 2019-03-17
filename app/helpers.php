2f2e9374d0a33f9869a6958857910075e34af4b7
yxj0312 <yanxiajun@hotmail.com>
2019-03-17 21:34:49 +0000

    Apply fixes from StyleCI

--- app/helpers.php
+++ app/helpers.php
@@ -9,9 +9,8 @@ function gravatar_url($email)
 {
     $email = md5($email);
 
-    return "https://gravatar.com/avatar/{$email}?" . http_build_query([
+    return "https://gravatar.com/avatar/{$email}?".http_build_query([
         's' => 60,
-        'd' => 'https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg'
+        'd' => 'https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg',
     ]);
 }
-
