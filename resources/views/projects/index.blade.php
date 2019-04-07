09b1f7aa3c66639309753b7f952d27ea9ee276f4
yxj0312 <yanxiajun@hotmail.com>
2019-04-07 22:37:47 +0200

    Ep 36 Easy Tailwind Themes Using CSS Variables

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
-@section('content')    
+@section('content')
     <header class="flex items-center mb-3 pb-4">
         <div class="flex justify-between items-end w-full">
-            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+            <h2 class="text-muted text-base font-light">My Projects</h2>
+
             <a href="/projects/create" class="button">New Project</a>
         </div>
     </header>
@@ -11,7 +12,7 @@
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                @include('projects.card')
+                @include ('projects.card')
             </div>
         @empty
             <div>No projects yet.</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,27 +1,27 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
 @section('content')
-    <header class="flex items-center mb-3 pb-4">
+    <header class="flex items-center mb-6 pb-4">
         <div class="flex justify-between items-end w-full">
-            <p class="text-grey text-sm font-normal">
-                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a>
+            <p class="text-muted font-light">
+                <a href="/projects" class="text-muted no-underline hover:underline">My Projects</a>
                 / {{ $project->title }}
             </p>
 
             <div class="flex items-center">
                 @foreach ($project->members as $member)
-                    <img 
-                        src="{{ gravatar_url($member->email) }}" 
-                        alt="{{ $member->name }}'s avatar" 
+                    <img
+                        src="{{ gravatar_url($member->email) }}"
+                        alt="{{ $member->name }}'s avatar"
                         class="rounded-full w-8 mr-2">
                 @endforeach
 
-                <img 
-                    src="{{ gravatar_url($project->owner->email) }}" 
-                    alt="{{ $project->owner->name }}'s avatar" 
+                <img
+                    src="{{ gravatar_url($project->owner->email) }}"
+                    alt="{{ $project->owner->name }}'s avatar"
                     class="rounded-full w-8 mr-2">
 
-                <a href="{{ $project->path().'/edit' }}" class="button ml-4">Update Project</a>
+                <a href="{{ $project->path().'/edit' }}" class="button ml-4">Edit Project</a>
             </div>
         </div>
     </header>
@@ -30,7 +30,7 @@
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    <h2 class="text-lg text-muted font-light mb-3">Tasks</h2>
 
                     {{-- tasks --}}
                     @foreach ($project->tasks as $task)
@@ -40,8 +40,8 @@
                                 @csrf
 
                                 <div class="flex items-center">
-                                    <input type="text" name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey' : ''}}">              
-                                    <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
+                                    <input name="body" value="{{ $task->body }}" class="text-default bg-card w-full {{ $task->completed ? 'line-through text-muted' : '' }}">
+                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
                             </form>
                         </div>
@@ -50,41 +50,40 @@
                     <div class="card mb-3">
                         <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
-                            <input type="text" placeholder="Add a new task..." class="w-full" name="body">
 
+                            <input placeholder="Add a new task..." class="text-default bg-card w-full" name="body">
                         </form>
-                    </div>                       
-                               
+                    </div>
                 </div>
 
-                <div class="mb-8">
-                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                <div>
+                    <h2 class="text-lg text-muted font-light mb-3">General Notes</h2>
 
                     {{-- general notes --}}
-                    <form action="{{ $project->path() }}" method="post">
+                    <form method="POST" action="{{ $project->path() }}">
                         @csrf
                         @method('PATCH')
 
-                        <textarea 
-                            class="card w-full mb-4" 
-                            style="min-height: 200px" 
-                            placeholder="Anything special that you want to make a note of?"
+                        <textarea
                             name="notes"
+                            class="card text-default w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
                         >{{ $project->notes }}</textarea>
 
                         <button type="submit" class="button">Save</button>
                     </form>
 
-                    @include('errors')
+                    @include ('errors')
                 </div>
             </div>
 
             <div class="lg:w-1/4 px-3 lg:py-8">
-                @include('projects.card')
-                @include('projects.activity.card')
+                @include ('projects.card')
+                @include ('projects.activity.card')
 
-                @can('manage', $project)
-                    @include('projects.invite')
+                @can ('manage', $project)
+                    @include ('projects.invite')
                 @endcan
             </div>
         </div>
