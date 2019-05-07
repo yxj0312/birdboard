f22478ce725da8893ada4056caee17547fe6894c
yxj0312 <yanxiajun@hotmail.com>
2019-05-07 21:52:53 +0200

    Ep 43 02:39

---
+++ resources/js/components/Dropdown.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="dropdown">
+        <!-- trigger -->
+        <!-- Each of this will be unique of the dropdown, so we use slot here -->
+        <slot name="trigger"></slot>
+
+        <!-- menu links -->
+        <div class="dropdown-menu">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [],
+
+        components: {},
+
+        data() {
+            return {
+                isOpen: false           
+            }
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            
+        }
+    }
+</script>
